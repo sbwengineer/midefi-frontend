cdaec122a3fe9982e64f67bfc412a35081b8befb
spider1998 <spider19981119@outlook.com>
2022-04-07 12:04:42 -0700

    final

--- src/components/dashboard/TransactionList.vue
+++ src/components/dashboard/TransactionList.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="transaction-list">
-    <h2 class="ma-0 d-flex align-center">Transaction History:</h2>
-    <div class="pt-1">
-      <v-divider class="divider-search" />
-    </div>
+    <h2 class="ma-0 d-flex align-center">Transaction History</h2>
     <v-data-table
       dense
       :headers="headers"
@@ -12,7 +9,6 @@
       class="elevation-1"
       :custom-filter="filterOnlyCapsText"
       :item-class="itemRowBackground"
-      :search="toSearch"
       :footer-props="{
         'items-per-page-options': [3, 5, 10, 20],
       }"
@@ -20,20 +16,11 @@
     >
       <template slot="no-data">No history around here </template>
 
-      <template v-slot:top>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          class="mx-4"
-        ></v-text-field>
-      </template>
-
-      <template v-slot:[`item.hash`]="{ item }">
+      <!-- <template v-slot:[`item.hash`]="{ item }">
         <a target="_blank" :href="rskExplorerUrl(item.hash)">{{
           (!item.hash ? '0x0' : item.hash) | formatHash
         }}</a>
-      </template>
+      </template> -->
       <template v-slot:[`item.date`]="{ item }">
         {{ new Date(item.date).toISOString().split('T')[0] }}
       </template>
@@ -74,10 +61,6 @@ export default {
           text: 'Date',
           value: 'date',
           align: 'start',
-        },
-        {
-          text: 'Hash',
-          value: 'hash',
         },
         { text: 'Tokens', value: 'price' },
         { text: 'Tx Event', value: 'type' },
--- src/views/MyActivity.vue
+++ src/views/MyActivity.vue
@@ -1,253 +1,55 @@
 <template>
   <div class="my-activity">
     <div v-if="dataLoaded" class="upper-banner">
-      <v-dialog v-model="showHealthWarning" width="450">
-        <v-card class="container">
-          <v-row class="ma-0 py-2 d-flex justify-center">
-            <v-icon color="#000000" large>report_problem</v-icon>
-          </v-row>
-          <v-row class="ma-0 py-2 d-flex justify-center">
-            <h1 class="blackish">Watch out!</h1>
-          </v-row>
-          <v-row class="ma-0 pt-6 d-flex justify-center">
-            <p class="text-center">
-              Your Health Factor has dropped to <span class="redish"> 0%</span>!
-            </p>
-          </v-row>
-          <v-row class="ma-0 px-3 d-flex justify-center">
-            <p class="text-center">
-              All your collaterals will be liquidated. If you wish to continue trading on MIDeFi,
-              feel free to supply new markets on the network.
-            </p>
-          </v-row>
-        </v-card>
-      </v-dialog>
-      <v-row class="d-flex justify-center">
-        <h1 class="my-5">Investment Dashboard</h1>
-      </v-row>
-      <!-- account balance -->
-      <v-row class="ma-0 py-2">
-        <v-col class="pa-0">
-          <v-row class="ma-0 py-0">
-            <v-col class="pa-0 d-flex justify-center">
-              <v-card class="graphics-card container" width="94%">
-                <v-row class="ma-0">
-                  <v-col cols="12" class="py-0">
-                    <v-row
-                      v-for="item in totalBalance"
-                      :key="item.toString().concat('balance')"
-                      class="d-flex align-center"
-                    >
-                      <v-col cols="6" class="ma-0 d-flex align-center px-0"
-                        ><h2>Account balance:</h2></v-col
-                      >
-                      <v-col cols="5" class="px-0">
-                        <h2 class="text-center">
-                          $
-                          <number
-                            ref="tweenedTotalBalance"
-                            class="tweened-number-blue"
-                            :from="item"
-                            :to="item"
-                            :format="tweenedFormat"
-                            :duration="5"
-                            :delay="1"
-                            easing="Power0.easeIn"
-                          />
-                        </h2>
-                      </v-col>
-                      <v-col cols="1" class="pa-0"><span class="text-left">USD</span></v-col>
-                    </v-row>
-                    <v-row class="d-flex align-center mt-0 pt-1">
-                      <v-divider />
-                    </v-row>
-                    <v-row v-for="(item, index) in totalSupplied" :key="`totalSupplied-${index}`">
-                      <v-col cols="6" class="px-0">
-                        <router-link :to="{ name: 'SupplyBorrow', params: { tab: 'supply' } }">
-                          <h4 :style="{ textDecoration: 'underline' }">Total Supplied:</h4>
-                        </router-link>
-                      </v-col>
-                      <v-col cols="5" class="px-0">
-                        <h4 class="text-center">
-                          $
-                          <number
-                            ref="tweenedTotalSupply"
-                            class="tweened-number"
-                            :from="item"
-                            :to="item"
-                            :format="tweenedFormat"
-                            :duration="5"
-                            :delay="1"
-                            easing="Power0.easeIn"
-                          />
-                        </h4>
-                      </v-col>
-                      <v-col cols="1" class="pa-0"><span class="text-left">USD</span></v-col>
-                    </v-row>
-                    <v-row v-for="item in totalBorrowed" :key="item">
-                      <v-col cols="6" class="px-0">
-                        <router-link :to="{ name: 'SupplyBorrow', params: { tab: 'borrow' } }">
-                          <h4 :style="{ textDecoration: 'underline' }">Total Borrowed:</h4>
-                        </router-link>
-                      </v-col>
-                      <v-col cols="5" class="px-0">
-                        <h4 class="text-center">
-                          $
-                          <number
-                            ref="tweenedTotalBorrow"
-                            class="tweened-number"
-                            :from="item"
-                            :to="item"
-                            :format="tweenedFormat"
-                            :duration="5"
-                            :delay="1"
-                            easing="Power0.easeIn"
-                          />
-                        </h4>
-                      </v-col>
-                      <v-col cols="1" class="pa-0"><span class="text-left">USD</span></v-col>
-                    </v-row>
-                  </v-col>
-                </v-row>
-              </v-card>
-            </v-col>
-          </v-row>
-          <v-row class="ma-0 py-2">
-            <!-- halth factor -->
-            <v-col class="pa-0 d-flex justify-center">
-              <v-card class="graphics-card d-flex align-center" width="94%">
-                <v-row class="ma-0">
-                  <v-col cols="7">
-                    <v-row class="ma-0 d-flex align-center">
-                      <h2>Health Factor:</h2>
-                      <v-tooltip top color="#E5E5E5">
-                        <template v-slot:activator="{ on, attrs }">
-                          <v-icon small class="mx-5" v-bind="attrs" v-on="on">info</v-icon>
-                        </template>
-                        <div class="tooltip">
-                          Your Health Factor represents <br />
-                          the state of your loans.
-                          <span class="boldie">
-                            Don't <br />
-                            let it drop to
-                            <span class="redish"> 0% </span></span
-                          >
-                          or your <br />
-                          collateral might be liquidated!
-                        </div>
-                      </v-tooltip>
-                    </v-row>
-                    <v-row class="ma-0 pt-1">
-                      <v-divider />
-                    </v-row>
-                    <v-row class="ma-0 pt-3">
-                      <p>
-                        Your account has a <b>{{ risk }}</b> risk of liquidation
-                      </p>
-                    </v-row>
-                  </v-col>
-                  <v-col cols="5" class="d-flex justify-center">
-                    <v-progress-circular
-                      :rotate="270"
-                      :size="150"
-                      :width="25"
-                      :value="healthFactor"
-                      color="#536DFE"
-                    >
-                      {{ accountHealth }}%
-                    </v-progress-circular>
-                  </v-col>
-                </v-row>
-              </v-card>
-            </v-col>
-          </v-row>
+      <v-card class="graphics-card d-flex align-center card-1" width="1114px" height="151px">
+        <v-col cols="4" class="d-flex supp">
+          <img v-img :src="require('@/assets/myActivity/1.png')" />
+          <p class="supp-1">Supplied</p>
+          <h1 class="supp-2">$198K</h1>
         </v-col>
-        <v-col class="pa-0">
-          <!-- supply borrow graph -->
-          <v-card class="graphics-card container" width="94%">
-            <SupplyBorrowGraph />
-          </v-card>
+        <v-col cols="4" class="d-flex justify-center border call">
+          <img v-img :src="require('@/assets/myActivity/2.png')" />
+          <p class="call-1">Callateral</p>
+          <h1 class="call-2">$2.4K</h1>
         </v-col>
-      </v-row>
-      <v-row class="ma-0 py-2">
-        <!-- markets prices -->
-        <v-col class="pa-0 d-flex justify-center">
-          <v-card class="graphics-card container" width="94%">
-            <v-row class="ma-0 d-flex align-center">
-              <v-col cols="12" class="py-0">
-                <v-row class="d-flex align-center">
-                  <v-col cols="9" class="ma-0 d-flex align-center"><h2>Markets Prices:</h2></v-col>
-                </v-row>
-                <v-row class="ma-0 pt-1">
-                  <v-divider />
-                </v-row>
-                <v-row v-for="item in markets" :key="item.symbol" class="ma-0 d-flex align-center">
-                  <v-col cols="3" class="px-0 text-center market-price-icon">
-                    <v-list-item-avatar tile size="30">
-                      <v-img
-                        :src="require(`@/assets/tokens/${item.logo}.png`)"
-                      /> </v-list-item-avatar
-                  ></v-col>
-                  <v-col cols="3" class="px-0 text-center market-price-icon">
-                    <v-list-item-avatar tile size="30">
-                      <v-img
-                        v-if="item.priceUp > 0"
-                        :src="require(`@/assets/myActivity/arrow_circle_up.svg`)"
-                      />
-                      <v-img
-                        v-else-if="item.price < 0"
-                        :src="require(`@/assets/myActivity/arrow_circle_down.svg`)"
-                      />
-                      <v-img
-                        v-else
-                        :src="require(`@/assets/myActivity/equal_circle.svg`)"
-                      /> </v-list-item-avatar
-                  ></v-col>
-                  <v-col cols="3" class="px-0 text-no-wrap text-left market-price">
-                    <h4 class="text-left">
-                      $
-                      <number
-                        ref="number1"
-                        class="tweened-number"
-                        :from="item.oldPrice"
-                        :to="item.price"
-                        :format="tweenedFormat"
-                        :duration="5"
-                        :delay="1"
-                        easing="Power0.easeIn"
-                      />
-                    </h4>
-                  </v-col>
-                  <v-col cols="2" class="pa-0 ml-2"><span class="text-left">USD</span></v-col>
-                </v-row>
-              </v-col>
+        <v-col cols="4" class="d-flex justify-right borr">
+          <img v-img :src="require('@/assets/myActivity/3.png')" />
+          <p class="borr-1">Borrowed</p>
+          <h1 class="borr-2">$89K</h1>
+        </v-col>
+      </v-card>
+      <!-- halth factor -->
+      <v-row>
+        <v-col cols="3">
+          <v-card class="graphics-card d-flex align-center health" width="304px" height="378px">
+            <v-row class="d-flex align-center text-health">
+              <h2>Health Factor</h2>
+            </v-row>
+            <v-row class="circular">
+              <v-progress-circular
+                :rotate="270"
+                :size="181"
+                :width="25"
+                :value="healthFactor"
+                color="#536DFE"
+              >
+                {{ accountHealth }}%
+              </v-progress-circular>
             </v-row>
           </v-card>
         </v-col>
-        <!-- history tx  -->
-        <v-col class="pa-0">
-          <v-card class="graphics-card container" width="94%">
+        <v-col class="pa-0 history">
+          <v-card class="graphics-card container" width="737px" height="368px">
             <TransactionList />
           </v-card>
         </v-col>
       </v-row>
-
-      <v-row class="ma-0"> </v-row>
     </div>
-    <template v-else>
-      <v-card flat class="loader d-flex align-center justify-center">
-        <div class="container">
-          <v-row class="my-5 d-flex justify-center">
-            <v-progress-circular indeterminate :size="80" color="#3e954a" />
-          </v-row>
-        </div>
-      </v-card>
-    </template>
   </div>
 </template>
 
 <script>
-import SupplyBorrowGraph from '@/components/dashboard/SupplyBorrowGraph.vue'
+// import SupplyBorrowGraph from '@/components/dashboard/SupplyBorrowGraph.vue'
 import TransactionList from '@/components/dashboard/TransactionList.vue'
 import { mapState } from 'vuex'
 import { cTokensDetails } from '../middleware/constants'
@@ -259,7 +61,7 @@ Vue.use(VueNumber)
 export default {
   name: 'MyActivity',
   components: {
-    SupplyBorrowGraph,
+    // SupplyBorrowGraph,
     TransactionList,
   },
   data() {
@@ -272,7 +74,7 @@ export default {
       polling: null,
       markets: [],
       accountStorage: '',
-      dataLoaded: false,
+      dataLoaded: true,
     }
   },
   computed: {
